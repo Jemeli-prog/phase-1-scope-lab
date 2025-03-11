e1c4af331edb1445931550b21918330348c38ae6
Ruth Kiptoo <ruthkiptoo523@gmail.com>
2025-03-11 09:24:14 +0300

    added assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    console.log(bestCustomer);
+ 
+
+}
+function overwriteBestCustomer() {
+      bestCustomer = "maybe bob";
+
+}
+
+const leastFavoriteCustomer = "Eva";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Gerry";
+
+}
+/*  describe('overwriteBestCustomer()', function() {
+    it('overwrites the best customer', function() {
+      overwriteBestCustomer('maybe bob');
+
+      expect(window.bestCustomer).to.equal('maybe bob');
+    });
+  });*/
